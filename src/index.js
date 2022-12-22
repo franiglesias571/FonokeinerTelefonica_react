fd3454f951461eff44ec3f51066854bebc33d940
franiglesias571 <franiglesias571@gmail.com>
2022-12-21 23:08:41 -0300

    pre aplicar cartwidget

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root')); //unica vez que vamos utilizar getelementbyid
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
 root.render(
-
-<App /> //este es un componente, el componente padre
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
-
